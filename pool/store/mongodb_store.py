3b4332e7ad5590693ea3bffb98c38a5454909a32
nphan2004 <nphan2004@gmail.com>
2021-08-15 15:11:34 -0400

    check for empty list

--- pool/store/mongodb_store.py
+++ pool/store/mongodb_store.py
@@ -175,5 +175,7 @@ class MongoDbPoolStore(AbstractPoolStore):
         for item in results:
             ret.append((uint64(item['timestamp']), uint64(item['difficulty'])))
         
-        self.log.info(f"{launcher_id}, current difficulty : {ret[0][1]}, partials count : {len(ret)}")        
+        if(len(ret) > 0) :
+            self.log.info(f"{launcher_id}, current difficulty : {ret[0][1]}, partials count : {len(ret)}")        
+            
         return ret
